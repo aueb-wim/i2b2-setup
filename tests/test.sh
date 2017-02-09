6bee634b66955c63d487e7086c66ece93f0b989f
Mirco Nasuti <mirco.nasuti@chuv.ch>
2017-02-09 12:01:22 +0100

    add test section in captain + update test.sh to use Docker for tests + update circle.yml too

--- tests/test.sh
+++ tests/test.sh
@@ -1,26 +1,23 @@
 #!/usr/bin/env bash
 
-# Start DB container (if not on CircleCI)
-echo "Starting DB container..."
-if [ -z "$CIRCLECI" ] || [ "$CIRCLECI" = false ] ; then
-    db_docker_id=$(docker run -d -p 5432:5432 -e 'POSTGRES_PASSWORD=postgres' postgres)
+# Start DB container
+echo "Starting container..."
+db_docker_id=$(docker run -d hbpmip/i2b2-db)
 
-    # Wait for DB to be ready
-    echo "Waiting for DB to be ready..."
-    sleep 5  # TODO: replace this by a test
+# Query database
+echo "Waiting for the DB to be ready..."
+sleep 5
+out=$(docker exec -ti ${db_docker_id} bash -c "psql -U postgres -c \"\dt\" | grep alembic_version")
+ret=${#out}
+
+# Remove DB container
+echo "Removing DB container..."
+docker kill ${db_docker_id}
+docker rm -f ${db_docker_id}
+
+if [ $ret -eq 48 ]
+then
+  exit 0
+else
+  exit 1
 fi
-
-echo "Changing directory..."
-cd ..
-echo "Running alembic migration..."
-alembic upgrade head
-ret=$?
-
-# Remove DB container (if not on CircleCI)
-if [ -z "$CIRCLECI" ] || [ "$CIRCLECI" = false ] ; then
-    echo "Removing DB container..."
-    docker kill ${db_docker_id}
-    docker rm -f ${db_docker_id}
-fi
-
-exit "$ret"
