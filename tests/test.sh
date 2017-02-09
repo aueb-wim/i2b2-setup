d6b3387ada25df00699962c565cfe4fe3dbd328e
Mirco Nasuti <mirco.nasuti@chuv.ch>
2017-02-09 13:18:56 +0100

    do not use docker on CircleCI

--- tests/test.sh
+++ tests/test.sh
@@ -1,31 +1,37 @@
 #!/usr/bin/env bash
 
-# Start DB container
-echo "Starting container..."
-db_docker_id=$(docker run -d hbpmip/i2b2-db)
 
-# Query database
-echo "Waiting for the DB to be ready..."
-sleep 5
-
-# For the moment, CircleCI still uses the LXC driver
 if [ -z "$CIRCLECI" ] || [ "$CIRCLECI" = false ] ; then
-  out=$(docker exec -ti ${db_docker_id} bash -c "psql -U postgres -c \"\dt\" | grep alembic_version")
-else
-  out=$(sudo lxc-attach -n "$(docker inspect --format "{{.Id}}" ${db_docker_id})" -- bash -c "psql -U postgres -c \"\dt\" | grep alembic_version")
-fi
-ret=${#out}
 
-# Remove DB container (if not in CircleCI)
-if [ -z "$CIRCLECI" ] || [ "$CIRCLECI" = false ] ; then
+    # Start DB container
+    echo "Starting container..."
+    db_docker_id=$(docker run -d hbpmip/i2b2-db)
+
+    # Query database
+    echo "Waiting for the DB to be ready..."
+    sleep 5
+    out=$(docker exec -ti ${db_docker_id} bash -c "psql -U postgres -c \"\dt\" | grep alembic_version")
+    ret=${#out}
+
+    # Remove DB container
     echo "Removing DB container..."
     docker kill ${db_docker_id}
     docker rm -f ${db_docker_id}
-fi
 
-if [ $ret -eq 48 ]
-then
-  exit 0
+    # Exit
+    if [ $ret -eq 48 ]
+    then
+      exit 0
+    else
+      exit 1
+    fi
+
 else
-  exit 1
+
+    # Do not use Docker on CircleCI because it uses LXC driver
+    echo "Changing directory..."
+    cd ..
+    echo "Running alembic migration..."
+    alembic upgrade head
+    exit $?
 fi
