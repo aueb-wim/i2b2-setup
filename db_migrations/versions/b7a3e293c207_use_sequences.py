60cf54c748d110752923408ebe3c7167e774e3d5
Mirco Nasuti <mirco.nasuti@chuv.ch>
2017-06-15 11:48:45 +0200

    generate psql sequences

---
+++ db_migrations/versions/b7a3e293c207_use_sequences.py
@@ -0,0 +1,40 @@
+"""use sequences
+
+Revision ID: b7a3e293c207
+Revises: 0bde60e6657f
+Create Date: 2017-06-15 11:43:35.574932
+
+"""
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'b7a3e293c207'
+down_revision = '0bde60e6657f'
+branch_labels = None
+depends_on = None
+
+
+# Create Postgresql sequences
+
+def create_seq(name):
+    op.execute(sa.schema.CreateSequence(sa.Sequence(name)))
+
+create_seq('patient_num_seq')
+create_seq('encounter_num_seq')
+
+
+def upgrade():
+    op.alter_column("encounter_mapping", "encounter_num",
+                    nullable=False, server_default=sa.text("nextval('encounter_num_seq'::regclass)"))
+    op.alter_column("patient_mapping", "patient_num",
+                    nullable=False, server_default=sa.text("nextval('patient_num_seq'::regclass)"))
+
+
+def downgrade():
+    op.alter_column("encounter_mapping", "encounter_num",
+                    nullable=True, server_default=None)
+    op.alter_column("patient_mapping", "patient_num",
+                    nullable=True, server_default=None)
